25213105153f1f2332657094a94e892aee535ad2
Birka <asus.rok@gmail.com>
2021-09-11 22:00:47 -0300

    funcionalidad a los botones
    funcionalidad a los botones +  icons

--- app/src/main/java/com/BirkaCode/BirkaNews/MainActivity.java
+++ app/src/main/java/com/BirkaCode/BirkaNews/MainActivity.java
@@ -3,12 +3,55 @@ package com.BirkaCode.BirkaNews;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean estadoBoton;
+    Button boton;
+    TextView noticias;
+    TextView tituloNoticia;
+    ImageView imagen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        estadoBoton = true;
+        boton = findViewById(R.id.btnSiguiente);
+        noticias = findViewById(R.id.noticias);
+        tituloNoticia = findViewById(R.id.tituloNoticia);
+        imagen = findViewById(R.id.imagenNoticia);
+
     }
+
+    public void siguienteNoticia(View mostrar) {
+        if (estadoBoton == true) {
+            tituloNoticia.setText("Titulo Noticia N°4");
+            noticias.setText(
+                    "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.");
+            estadoBoton = false;
+        } else {
+            tituloNoticia.setText("Titulo Noticia N°5");
+            noticias.setText("There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.");
+            estadoBoton = true;
+        }
+    }
+
+    public void anteriorNoticia(View mostrar) {
+        if (estadoBoton == true) {
+            tituloNoticia.setText("Titulo Noticia N°3");
+            noticias.setText(
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+            estadoBoton = false;
+        } else {
+            tituloNoticia.setText("Titulo Noticia N°2");
+            noticias.setText("The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.");
+            estadoBoton = true;
+        }
+    }
+
 }
